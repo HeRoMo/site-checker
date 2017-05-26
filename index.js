d91528466d9f6b229891ec1ad6b47938087f2f3b
HeRoMo <hero@asterisk-works.jp>
2017-05-27 01:25:49 +0900

    add config parameters

--- index.js
+++ index.js
@@ -4,45 +4,66 @@ const CDP = require('chrome-remote-interface');
 const file = require('fs');
 const readCsv = require('./lib/read_csv.js');
 
-const delay = 0;
-const format = 'png'
+const verbose = false;
 
 function sleep(sec) {
   return new Promise(resolve => setTimeout(resolve, sec*1000));
 }
 
-function checkAndCapture(list){
+const defaultOptions = {
+  format: 'png',
+  viewportWidth: 1280,
+  viewportHeight: 800,
+  delay: 0,
+  userAgent: null,
+  fullPage: false
+}
+
+function checkAndCapture(list, options = defaultOptions){
   return new Promise(function(resolve, reject){
     CDP(async function(client){
       const output = {}
       const {DOM, Emulation, Network, Page, Runtime} = client;
+      const deviceMetrics = {
+        width: options.viewportWidth || defaultOptions.viewportWidth,
+        height: options.viewportHeight || defaultOptions.viewportHeight,
+        screenWidth: options.viewportWidth || defaultOptions.viewportWidth,
+        screenHeight: options.viewportHeight || defaultOptions.viewportHeight,
+        offsetX: 0,
+        offsetY: 0,
+        deviceScaleFactor: 0,
+        mobile: false,
+        fitWindow: true,
+      }
       await Promise.all([
         Network.enable(),
         Page.enable(),
-        DOM.enable()
+        DOM.enable(),
+        Emulation.setDeviceMetricsOverride(deviceMetrics),
+        Emulation.setVisibleSize({width: options.viewportWidth, height: options.viewportHeight})
       ])
       await Network.clearBrowserCache()
 
       Network.requestWillBeSent((params) => {
         if (params.type == 'Document' && output[params.requestId]) {
-          console.log("Sent: %s %s",params.requestId, params.documentURL);
           output[params.requestId]['requestUrl']=params.documentURL
+          if(verbose) console.log("Sent: %s %s",params.requestId, params.documentURL);
         }
       });
 
       Network.responseReceived((res) => {
         if (res.type == 'Document' && output[res.requestId] ) {
-          console.log("Resp: %s %s", res.requestId ,res.response.status, res.response.url)
           output[res.requestId]['status']=res.response.status
           output[res.requestId]['responseUrl']=res.response.url
+          if(verbose) console.log("Resp: %s %s", res.requestId ,res.response.status, res.response.url);
         }
       })
 
       Network.loadingFailed((res) => {
         if (res.type == 'Document' && output[res.requestId]){
-          console.log("Erro: %s %s", res.requestId, res.errorText)
           output[res.requestId]['status']='ERR'
           output[res.requestId]['errorText']=res.errorText
+          if(verbose) console.log("Erro: %s %s", res.requestId, res.errorText);
         }
       })
 
@@ -56,13 +77,28 @@ function checkAndCapture(list){
         let url = target.url
         try {
           let {frameId} = await Page.navigate({url})
-          console.log("Nav : %s %s",frameId,url)
+          if(verbose) console.log("Nav : %s %s",frameId,url)
           output[frameId]=target
           await Page.loadEventFired();
-          await sleep(delay);
-          const {data} = await Page.captureScreenshot({format});
-          let filename = "output_"+target.id+".png"
-          file.writeFileSync(filename, Buffer.from(data, 'base64'));
+          if (options.fullPage) {
+            const {root: {nodeId: documentNodeId}} = await DOM.getDocument();
+            const {nodeId: bodyNodeId} = await DOM.querySelector({
+              selector: 'body',
+              nodeId: documentNodeId,
+            });
+            const {model: {height}} = await DOM.getBoxModel({nodeId: bodyNodeId});
+            await Promise.all([
+              Emulation.setVisibleSize({width: options.viewportWidth, height: height}),
+              Emulation.forceViewport({x: 0, y: 0, scale: 1})
+            ]);
+          }
+          await sleep(options.delay);
+          if(target.status == 200){
+            const {data} = await Page.captureScreenshot({format: options.format});
+            let filename = "capture_"+target.id+".png"
+            file.writeFileSync(filename, Buffer.from(data, 'base64'));
+            target.capture = filename
+          }
         } catch(e) {
           console.log(e)
         }
